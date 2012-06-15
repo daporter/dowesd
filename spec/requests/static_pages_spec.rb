0c01496891bc3ed15572240c00c90de57a4a28d5
David Porter <david.a.porter@gmail.com>
2012-06-15 12:48:18 +1000

    Refactor specs

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,21 +1,38 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  let(:base_title) { "D Owes D" }
+describe 'StaticPages' do
+  let(:base_title) { 'D Owes D' }
 
   subject { page }
 
-  describe "About page" do
-    before { visit about_path }
-
-    it { should have_selector 'title', text: full_title('About') }
-    it { should have_selector 'h1',    text: 'About' }
+  it 'should have the right links on the layout' do
+    visit root_path
+    click_link 'About'
+    page.should have_selector 'title', text: full_title('About')
+    click_link 'Contact'
+    page.should have_selector 'title', text: full_title('Contact')
   end
 
-  describe "Contact page" do
+  shared_examples_for 'all static pages' do
+    it { should have_selector 'title', text: full_title(page_title) }
+    it { should have_selector 'h1',    text: heading }
+  end
+
+  describe 'About page' do
+    before { visit about_path }
+
+    let(:heading)    { 'About' }
+    let(:page_title) { 'About' }
+
+    it_should_behave_like 'all static pages'
+  end
+
+  describe 'Contact page' do
     before { visit contact_path }
 
-    it { should have_selector 'title', text: full_title('Contact') }
-    it { should have_selector 'h1',    text: 'Contact' }
+    let(:heading)    { 'Contact' }
+    let(:page_title) { 'Contact' }
+
+    it_should_behave_like 'all static pages'
   end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,4 +1,1 @@
-def full_title(page_title)
-  base_title = "D Owes D"
-  page_title.empty? ? base_title : "#{base_title} | #{page_title}"
-end
+include ApplicationHelper
