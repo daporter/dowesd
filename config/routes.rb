a1e996a91b21859b3548e9d4ded5afe6ac641d27
David Porter <david.a.porter@gmail.com>
2012-06-18 12:11:23 +1000

    Add signin and signout

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Dowesd::Application.routes.draw do
   match '/about',   :to => 'static_pages#about'
   match '/contact', :to => 'static_pages#contact'
 
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+
   resources :txns
 end
